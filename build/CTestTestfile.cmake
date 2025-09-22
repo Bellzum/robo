a52c4518314f5b48dcaf41aaf7ec68883dd15fdf
umbhorn.ung@gmail.com <umbhorn.ung@gmail.com>
2025-09-22 13:10:34 +0000

    initial commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/maiimao/catkin_ws/src
+# Build directory: /home/maiimao/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("robo/my_py_pkg")
---
+++ build/robo/my_py_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/maiimao/catkin_ws/src/robo/my_py_pkg
+# Build directory: /home/maiimao/catkin_ws/build/robo/my_py_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
